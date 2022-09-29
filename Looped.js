4da4c04a6a0eaa8de9b03421cebb839d5491f479
Tazz Hughes <105616501+TazzHughes@users.noreply.github.com>
2022-09-28 17:04:33 -0700

    Add files via upload

---
+++ Looped.js
@@ -0,0 +1,12 @@
+firstName = prompt("firstName");
+lastName = prompt("lastName");
+badgeNumber = prompt("badge Number")
+function myAlert() {
+    //for loop will run/countdown 10 times
+    for(count =10; count > -1; count--) {
+    //alerts will show the actual countdown/numbers
+        alert("count=" +count);
+    }
+}
+//calling the function
+myAlert();
